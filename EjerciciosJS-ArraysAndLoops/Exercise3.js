f10540790b7c94da2d5b47ef6062f488c04039ca
Jael Alonso Bohorquez <jaelbkz@icloud.com>
2025-02-08 19:32:13 -0600

    Update Exercise3.js

--- EjerciciosJS-ArraysAndLoops/Exercise3.js
+++ EjerciciosJS-ArraysAndLoops/Exercise3.js
@@ -1,1 +1,8 @@
+//Exercise 3: Write a function that takes an array and returns the sum of all elements in the array.
 
+function sumElementsInArray(arr){
+    return arr.reduce((sum, element) => sum + element, 0);
+}
+
+const arr = [1, 2, 3, 4];
+console.log(sumElementsInArray(arr));
